4ce12959a23ef9ce681dcc5c41296aeb8e611934
luismpaiva <luismpaiva@uninova.pt>
2015-11-18 10:51:17 +0000

    KpiValues data object update

--- dataServer/src/dataServer/database/dbobjects/KpiValues.java
+++ dataServer/src/dataServer/database/dbobjects/KpiValues.java
@@ -4,10 +4,11 @@ import java.sql.Timestamp;
 public class KpiValues extends KpiDataObject {
 	public Timestamp timestamp;
 	public int value;
-	public int kpiId;
 	public int machineId;
-	public int mouldId;
 	public int productId;
+	public String designation;
+	public boolean goodPart;
+	public int kpiId;
 	
 	public KpiValues() {
 		super("kpi_values");
