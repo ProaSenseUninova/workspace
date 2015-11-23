2fab6c5c0ef4412675dd1fdc417ef2c99cd01034
luismpaiva <luismpaiva@uninova.pt>
2015-11-23 14:15:48 +0000

    .

---
+++ dataServer/src/dataServer/database/enums/TableValueType.java
@@ -0,0 +1,15 @@
+package dataServer.database.enums;
+
+public enum TableValueType {
+	PRODUCT,
+	MOULD,
+	MACHINE,
+	SHIFT,
+	KPI_VALUES,
+	KPI_TARGET,
+	KPI, 
+	KPI_AGG_TYE,
+	KPI_FORMULA,
+	SENSOR
+
+}
